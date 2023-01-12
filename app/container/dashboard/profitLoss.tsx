4fa9eac4a2512e404f7af5f439029a38268eadad
andisulistyonugroho <andisulistyonugroho@gmail.com>
2023-01-12 11:42:49 +0700

    refactor: change table to grid box
    Table is overflowed to x-axis, so we use grid to stay in view

--- app/container/dashboard/profitLoss.tsx
+++ app/container/dashboard/profitLoss.tsx
@@ -1,16 +1,16 @@
 import {
   Box, Center, Text, Flex, Button, Spacer, useDisclosure, Modal,ModalOverlay,ModalContent, ModalHeader,
-  ModalCloseButton, ModalFooter, ModalBody, TableContainer, Table, Tbody, Tr, Td
+  ModalCloseButton, SimpleGrid, TableContainer, Table, Tbody, Tr, Td
 } from '@chakra-ui/react'
 
-interface profitloss {
+interface data {
   profitloss: {
     value: number,
     details: any
   }
 }
 
-function ProfitLoss(data: profitloss) {
+function ProfitLoss(data: data) {
     const profitloss = data.profitloss
     const grossOmset = profitloss.details.find((obj: { label: string }) => obj.label === 'Pendapatan Kotor')
     const expense = profitloss.details.find((obj: {label: string}) => obj.label === 'Beban')
@@ -41,16 +41,17 @@ function ProfitLoss(data: profitloss) {
             <Text color='red' fontSize='sm'>Rp {totalExpense}</Text>
           </Box>
         </Flex>
-        <DetailProfitLoss />
+        <DetailProfitLoss profitloss={profitloss} />
       </Box>
     )
 }
 
-function DetailProfitLoss() {
+function DetailProfitLoss(data:data) {
   const {onOpen, isOpen, onClose} = useDisclosure()
+
   return (
     <>
-    <Button size='sm' isFullWidth onClick={onOpen}>Lihat detail</Button>
+    <Button isFullWidth onClick={onOpen}>Lihat detail</Button>
     <Modal isOpen={isOpen} onClose={onClose} size='sm' isCentered>
       <ModalOverlay />
       <ModalContent fontFamily='Poppins'>
@@ -60,30 +61,31 @@ function DetailProfitLoss() {
         </ModalHeader>
         
         <ModalCloseButton />
+
+        <SimpleGrid columns={2} fontSize='sm'>
+        {data.profitloss.details.map((row: { label: string,value: number }, index:number) => (
+          <GridProfitLoss key={index} gridrow={row} rowctr={index} />
+        ))}
+        </SimpleGrid>
         
-        <TableContainer>
-          <Table variant='striped' colorScheme='teal' size='sm'>
-            <Tbody>
-              <Tr>
-                <Td fontWeight='bold' colSpan={3}>Pendapatan</Td>
-              </Tr>
-              <Tr>
-                <Td fontSize='xs'>
-                  Pendapatan Kotor
-                </Td>
-                <Td isNumeric fontSize='xs'>196919000</Td>
-                <Td></Td>
-              </Tr>
-              <Tr>
-                <Td fontSize='xs'>Diskon Penjualan</Td>
-                <Td></Td>
-                <Td isNumeric fontSize='xs'>196919000</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
       </ModalContent>
     </Modal>
+    </>
+  )
+}
+
+function formatNumber(value:number) {
+  return Intl.NumberFormat('en-US').format(value)
+}
+
+function GridProfitLoss(data:{ gridrow:{label: string,value: number},rowctr: number }) {
+  const rowColor = function(value:number) {
+    return value%2 === 1 ? 'teal.100' : ''
+  }
+  return (
+    <>
+    <Box bg={rowColor(data.rowctr)} py='2' pl='4'>{data.gridrow.label}</Box>
+    <Box bg={rowColor(data.rowctr)} py='2' pr='3'><Text align='right'>{formatNumber(data.gridrow.value)}</Text></Box>
     </>
   )
 }
